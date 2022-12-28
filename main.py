e025390c9050b3073c22d944499a87cd78691aed
Samantha Iraheta <sairaheta1@gmail.com>
2022-12-28 11:37:39 -0800

    created get_num_lines function to get the number of lines the user would like to bet on; created main function to run the program

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+#setting up constants for max lines and min lines
+MAX_LINES = 3
+def main():
+  deposit()
+  get_num_lines()
+
+
 #collect user input to get deposit from user
 def deposit ():
   while True:
@@ -12,5 +19,20 @@ def deposit ():
         break
   return amount
 
+def get_num_lines(): #ask user how many lines they would like to bet on
+  while True:
+    lines = input("How many lines would you like to bet on? Please enter a value between 1-" + str(MAX_LINES) + ". ")
 
-deposit()
+    if not lines.isdigit():
+      print("Please enter a valid number of lines for your bet.")
+    else:
+      lines = int(lines)
+      if lines > MAX_LINES or lines < 1:
+        print("Please enter a valid number of lines for your bet.")
+      else:
+        break
+  return lines
+
+
+
+main()
